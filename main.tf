93a0f4ba82ef31c87a5f87fb4ac27dd8eb143810
Cliff <clifpande@yahoo.com>
2023-01-29 01:57:15 -0500

    new

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "utrains-eks-${random_string.suffix.result}"
+  cluster_name = "cliffs-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
-  length  = 8
+  length  = 4
   special = false
 }
